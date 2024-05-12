d9618dcea6027d7333dc38e73c9431ab08bfbe52
ostapenko-kostia <ostapenkokpersonal@gmail.com>
2024-05-12 16:52:32 +0300

    Add categories slider

--- src/components/NewsFilters/NewsFilters.jsx
+++ src/components/NewsFilters/NewsFilters.jsx
@@ -6,20 +6,23 @@ import Search from "../../components/Search/Search";
 import useFetch from "../../helpers/hooks/useFetch";
 
 import { getCategories } from "../../api/apiNews";
+import Slider from "../Slider/Slider";
 
-export default function NewsFilters({filters, changeFilters}) {
+export default function NewsFilters({ filters, changeFilters }) {
     const { data: dataCategories } = useFetch(getCategories);
 
     return (
         <div className={classes.filters}>
             {dataCategories ? (
-                <Categories
-                    categories={["All", ...dataCategories.categories]}
-                    setSelectedCategory={(currentCategory) =>
-                        changeFilters("category", currentCategory)
-                    }
-                    selectedCategory={filters.category}
-                />
+                <Slider>
+                    <Categories
+                        categories={["All", ...dataCategories.categories]}
+                        setSelectedCategory={(currentCategory) =>
+                            changeFilters("category", currentCategory)
+                        }
+                        selectedCategory={filters.category}
+                    />
+                </Slider>
             ) : null}
 
             <Search
